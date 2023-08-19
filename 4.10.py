73f39a0124653f2d6fda17f7af4ae3d51f2f2769
codeinkhan <28754440+codeinkhan@users.noreply.github.com>
2023-08-19 23:55:27 +0530

    Add files via upload

---
+++ 4.10.py
@@ -0,0 +1,9 @@
+# 4-10. Slices: Using one of the programs you wrote in this chapter, add several Iines to the end of the program that do the following Print the message
+#            The first three items in the list are..Then use a slice to print the first three items from that program's list,
+#            Print the message Three items from the middle of the list are. Then use a slice to print three items from the middle of the list.
+#            Print the message The last three items in the list are. Then use a slice to print the last three items in the list.
+
+mylist=['Apple','Pineapple','Chikoo','Muskmelon','Banana','Mango','Blueberry',]
+print("The first three items in the list are ",mylist[0:3])
+print("Three items from the middle of the list are ",mylist[2:5])
+print("The last three items in the list are ",mylist[4:7])
