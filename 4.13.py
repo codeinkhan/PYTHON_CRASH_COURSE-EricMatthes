6f15cc5486986be49b22486d8641c6c4702ea310
codeinkhan <28754440+codeinkhan@users.noreply.github.com>
2023-08-30 12:35:22 +0530

    Add files via upload

---
+++ 4.13.py
@@ -0,0 +1,16 @@
+#4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five simple foods, and store them in a tuple. Use a for loop to print each food the restaurant offers.
+#           Try to modify one of the items, and make sure that Python rejects change. The restaurant changes its menu, replacing two of the items with different foods.
+#           Add a line that rewrites the tuple, and then use a for loop to print each of the items on the revised menu.
+
+foods=('Egg','Bread','Milk','Rice','Juice')
+for food in foods:
+    print(food)
+print('\n')
+
+#foods[0]= 'Sweet'
+#for food in foods:
+#    print(food)
+
+foods=('Sweet','Bread','Ice Cream','Rice','Juice')
+for food in foods:
+    print(food)
